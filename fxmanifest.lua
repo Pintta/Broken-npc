33a8d6245afca5bac6b9596766341c9f8f2a64b5
Pintta <69728770+Pintta@users.noreply.github.com>
2023-11-09 16:05:08 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,4 @@ lua54 'yes'
 game 'gta5'
 file 'popcycle.dat'
 data_file 'POPSCHED_FILE' 'popcycle.dat'
-client_script 'client.lua'
--- client_script 'NoLoops.lua' -- Is you choice what you using, client.lua or NoLoops.lua this same script.
--- client_script 'OnlyArea.lua' -- If you want only something are clear using this
+client_script 'pintta.lua'
