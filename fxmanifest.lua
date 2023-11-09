f22459fc659d9ddf788c037b0dfb296030963e1c
Pintta <69728770+Pintta@users.noreply.github.com>
2023-11-09 15:26:14 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,3 +4,4 @@ game 'gta5'
 file 'popcycle.dat'
 data_file 'POPSCHED_FILE' 'popcycle.dat'
 client_script 'client.lua'
+-- client_script 'NoLoops.lua' -- Is you choice what you using, client.lua or NoLoops.lua this same script.
