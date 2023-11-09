9086c0f5a31b093735f84b9ba3c61dd79adf246b
Pintta <69728770+Pintta@users.noreply.github.com>
2023-11-09 15:56:13 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,3 +5,4 @@ file 'popcycle.dat'
 data_file 'POPSCHED_FILE' 'popcycle.dat'
 client_script 'client.lua'
 -- client_script 'NoLoops.lua' -- Is you choice what you using, client.lua or NoLoops.lua this same script.
+-- client_script 'OnlyArea.lua' -- If you want only something are clear using this
